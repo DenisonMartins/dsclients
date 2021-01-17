81cb1adef8a019ba9c51752606cdf680144d6d07
Michael <michael.dl.mrtns@gmail.com>
2021-01-17 09:27:44 -0300

    WIP: Add entity Client

---
+++ backend/src/main/java/com/michaelmartins/dsclients/domain/entities/Client.java
@@ -0,0 +1,98 @@
+package com.michaelmartins.dsclients.domain.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "tb_client", uniqueConstraints = @UniqueConstraint(name = "UK_client_cpf", columnNames = "cpf"))
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+
+    private String cpf;
+    private BigDecimal income;
+    private Instant birthDate;
+    private Integer children;
+
+    public Client() {
+    }
+
+    public Client(Long id, String name, String cpf, BigDecimal income, Instant birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public BigDecimal getIncome() {
+        return income;
+    }
+
+    public void setIncome(BigDecimal income) {
+        this.income = income;
+    }
+
+    public Instant getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id.equals(client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
